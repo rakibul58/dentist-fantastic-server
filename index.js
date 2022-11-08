5769a208ff963a590099c5e475d03fc47bf69d1c
rakibul58 <rhrahi14@gmail.com>
2022-11-08 23:13:54 +0600

    database connected

--- index.js
+++ index.js
@@ -15,11 +15,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bebjeyw.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+
+        const serviceCollection = client.db('dentistFantastic').collection('services');
+
+        app.get('/services' , async(req , res)=>{
+            const query = {};
+            let cursor;
+            if(req.query.length){
+                const size = parseInt(req.query.length);
+                cursor = serviceCollection.find(query).limit(size);
+                
+            }
+            else{
+                cursor = serviceCollection.find(query);
+            }
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=>console.error(err))
 
 
 app.get('/' , (req , res)=> {
