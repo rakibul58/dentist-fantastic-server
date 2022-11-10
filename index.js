8cea5a459ac58ad5c435ea941811944bb8d99e78
rakibul58 <rhrahi14@gmail.com>
2022-11-10 14:17:49 +0600

    jwt decoded

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req , res , next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , function(err , decoded){
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'});
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         // service collection
@@ -64,7 +79,12 @@ async function run() {
         });
 
         //get all comments
-        app.get('/comments', async (req, res) => {
+        app.get('/comments', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            // console.log(decoded);
+            if(decoded.email!== req.query.email){
+                res.status(403).send({message: 'unauthorize access'})
+            }
             let query;
             if (req.query.email) {
                 const email = req.query.email;
